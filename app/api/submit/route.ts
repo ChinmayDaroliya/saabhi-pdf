1b5f06362f46d0322035c2080154c33a8d936fb6
Chinmay Daroliya <chinmaydaroliya@gmail.com>
2026-03-18 09:43:35 +0530

    changed the db from supabase to googlesheets

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -1,5 +1,19 @@
+// import { NextResponse } from 'next/server';
+// import { supabase } from '@/lib/supabase';
+
+// export async function POST(req: Request) {
+//   const { name, phone } = await req.json();
+
+//   if (!name || !phone) {
+//     return NextResponse.json({ error: 'Invalid data' }, { status: 400 });
+//   }
+
+//   await supabase.from('leads').insert([{ name, phone }]);
+
+//   return NextResponse.json({ success: true });
+// }
+
 import { NextResponse } from 'next/server';
-import { supabase } from '@/lib/supabase';
 
 export async function POST(req: Request) {
   const { name, phone } = await req.json();
@@ -8,7 +22,25 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: 'Invalid data' }, { status: 400 });
   }
 
-  await supabase.from('leads').insert([{ name, phone }]);
+  const id = crypto.randomUUID(); // generate id
+  const created_at = new Date().toISOString(); // timestamp
+
+  await fetch('https://sheetdb.io/api/v1/96s56d9acze2x?sheet=english', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      data: [
+        {
+          id,
+          name,
+          number: phone,
+          created_at,
+        },
+      ],
+    }),
+  });
 
   return NextResponse.json({ success: true });
 }
